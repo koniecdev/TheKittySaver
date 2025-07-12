0db9d8184b50479841d5787b2a0149b5e576d694
koniecdev <koniecdev@gmail.com>
2025-07-12 22:13:04 +0200

    strongly typed ids added

---
+++ src/BoundedContexts/AdoptionSystem/TheKittySaver.AdoptionSystem.Domain/Aggregates/PersonAggregate/IdentityId.cs
@@ -0,0 +1,4 @@
+﻿namespace TheKittySaver.AdoptionSystem.Domain.Aggregates.PersonAggregate;
+
+[StronglyTypedId(jsonConverter: StronglyTypedIdJsonConverter.SystemTextJson)]
+public partial struct IdentityId;
