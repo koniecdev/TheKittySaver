1d87378a47d61763e56a2da37708c494993f436f
koniecdev <koniecdev@gmail.com>
2025-07-20 23:51:52 +0200

    Generic repository abstraction added

---
+++ src/BoundedContexts/AdoptionSystem/TheKittySaver.AdoptionSystem.Domain/Core/Primitives/BuildingBlocks/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using TheKittySaver.AdoptionSystem.Domain.Core.Primitives.OptionMonad;
+
+namespace TheKittySaver.AdoptionSystem.Domain.Core.Primitives.BuildingBlocks;
+
+public interface IRepository<TAggregateRoot, in TAggregateRootId> 
+    where TAggregateRootId : struct
+    where TAggregateRoot : AggregateRoot<TAggregateRootId>
+{
+    Task<Maybe<TAggregateRoot>> GetByIdAsync(TAggregateRootId id, CancellationToken cancellationToken);
+    void Insert(TAggregateRoot entity);
+    void Remove(TAggregateRoot entity);
+}
