72cb62bb1b73371978b65da417a634844f5c75f7
Artur Koniec <a.koniec@cognitum.eu>
2025-12-07 01:41:14 +0100

    person responses created

---
+++ src/AdoptionSystem/TheKittySaver.AdoptionSystem.Contracts/Aggregates/PersonAggregate/Responses/PersonAddressResponse.cs
@@ -0,0 +1,18 @@
+﻿using TheKittySaver.AdoptionSystem.Contracts.Common;
+using TheKittySaver.AdoptionSystem.Primitives.Aggregates.PersonAggregate;
+using TheKittySaver.AdoptionSystem.Primitives.Enums;
+
+namespace TheKittySaver.AdoptionSystem.Contracts.Aggregates.PersonAggregate.Responses;
+
+public sealed record PersonAddressResponse(
+    AddressId Id,
+    PersonId PersonId,
+    CountryCode CountryCode,
+    string Name,
+    string PostalCode,
+    string Region,
+    string City,
+    string? Line) : ILinksResponse
+{
+    public IReadOnlyCollection<LinkDto> Links { get; set; } = [];
+}
---
+++ src/AdoptionSystem/TheKittySaver.AdoptionSystem.Contracts/Aggregates/PersonAggregate/Responses/PersonResponse.cs
@@ -0,0 +1,13 @@
+﻿using TheKittySaver.AdoptionSystem.Contracts.Common;
+using TheKittySaver.AdoptionSystem.Primitives.Aggregates.PersonAggregate;
+
+namespace TheKittySaver.AdoptionSystem.Contracts.Aggregates.PersonAggregate.Responses;
+
+public sealed record PersonResponse(
+    PersonId Id,
+    string Username,
+    string Email,
+    string PhoneNumber) : ILinksResponse
+{
+    public IReadOnlyCollection<LinkDto> Links { get; set; } = [];
+}
