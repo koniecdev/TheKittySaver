defb08b98fa23a6dc3df2fc84296fd5040ae2735
Artur Koniec <a.koniec@cognitum.eu>
2026-01-04 23:39:11 +0100

    update cat tests

--- tests/TheKittySaver.AdoptionSystem.API.Tests.Integration/Tests/Cats/UpdateCatEndpointsTests.cs
+++ tests/TheKittySaver.AdoptionSystem.API.Tests.Integration/Tests/Cats/UpdateCatEndpointsTests.cs
@@ -1,62 +1,81 @@
-// using System.Net;
-// using System.Net.Http.Json;
-// using Microsoft.AspNetCore.Http;
-// using Microsoft.AspNetCore.Mvc;
-// using Shouldly;
-// using TheKittySaver.AdoptionSystem.API.Tests.Integration.Extensions;
-// using TheKittySaver.AdoptionSystem.API.Tests.Integration.Shared;
-// using TheKittySaver.AdoptionSystem.API.Tests.Integration.Shared.Factories;
-// using TheKittySaver.AdoptionSystem.Contracts.Aggregates.CatAggregate.Requests;
-// using TheKittySaver.AdoptionSystem.Contracts.Aggregates.CatAggregate.Responses;
-//
-// namespace TheKittySaver.AdoptionSystem.API.Tests.Integration.Tests.Cats;
-//
-// public sealed class UpdateCatEndpointsTests(TheKittySaverApiFactory appFactory)
-//     : CatEndpointsTestBase(appFactory)
-// {
-//     [Fact]
-//     public async Task UpdateCat_ShouldMapEveryRequestProperty_WhenValidDataIsProvided()
-//     {
-//         //Arrange
-//         CatDetailsResponse cat = await CatApiFactory.CreateRandomAsync(ApiClient, Faker, TestPersonId);
-//         UpdateCatRequest request = CatApiFactory.GenerateRandomUpdateRequest(Faker);
-//
-//         //Act
-//         HttpResponseMessage httpResponseMessage =
-//             await ApiClient.Http.PutAsJsonAsync(
-//                 new Uri($"api/v1/cats/{cat.Id}", UriKind.Relative), request);
-//
-//         //Assert
-//         _ = await httpResponseMessage.EnsureSuccessWithDetailsAsync();
-//         httpResponseMessage.StatusCode.ShouldBe(HttpStatusCode.NoContent);
-//
-//         CatDetailsResponse catResponse = await CatApiQueryService.GetByIdAsync(ApiClient, cat.Id);
-//         catResponse.ShouldNotBeNull();
-//         catResponse.Name.ShouldBe(request.Name);
-//         catResponse.Description.ShouldBe(request.Description);
-//         catResponse.Age.ShouldBe(request.Age);
-//         catResponse.Gender.ShouldBe(request.Gender);
-//         catResponse.Color.ShouldBe(request.Color);
-//         catResponse.HealthStatus.ShouldBe(request.HealthStatus);
-//         catResponse.Temperament.ShouldBe(request.Temperament);
-//     }
-//
-//     [Fact]
-//     public async Task UpdateCat_ShouldReturnNotFound_WhenNotExistingCatIdIsProvided()
-//     {
-//         //Arrange
-//         UpdateCatRequest request = CatApiFactory.GenerateRandomUpdateRequest(Faker);
-//
-//         //Act
-//         HttpResponseMessage httpResponseMessage =
-//             await ApiClient.Http.PutAsJsonAsync(
-//                 new Uri($"api/v1/cats/{Guid.NewGuid()}", UriKind.Relative), request);
-//
-//         //Assert
-//         httpResponseMessage.StatusCode.ShouldBe(HttpStatusCode.NotFound);
-//         ProblemDetails? problemDetails =
-//             await httpResponseMessage.Content.ReadFromJsonAsync<ProblemDetails>(ApiClient.JsonOptions);
-//         problemDetails.ShouldNotBeNull();
-//         problemDetails.Status.ShouldBe(StatusCodes.Status404NotFound);
-//     }
-// }
+using System.Net;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shouldly;
+using TheKittySaver.AdoptionSystem.API.Tests.Integration.Extensions;
+using TheKittySaver.AdoptionSystem.API.Tests.Integration.Shared;
+using TheKittySaver.AdoptionSystem.API.Tests.Integration.Shared.Factories;
+using TheKittySaver.AdoptionSystem.Contracts.Aggregates.CatAggregate.Requests;
+using TheKittySaver.AdoptionSystem.Contracts.Aggregates.CatAggregate.Responses;
+using TheKittySaver.AdoptionSystem.Primitives.Aggregates.CatAggregate;
+using TheKittySaver.AdoptionSystem.Primitives.Aggregates.PersonAggregate;
+
+namespace TheKittySaver.AdoptionSystem.API.Tests.Integration.Tests.Cats;
+
+public sealed class UpdateCatEndpointsTests(TheKittySaverApiFactory appFactory)
+    : CatEndpointsTestBase(appFactory)
+{
+    [Fact]
+    public async Task UpdateCat_ShouldMapEveryRequestProperty_WhenValidDataIsProvided()
+    {
+        //Arrange
+        PersonId personId = await PersonApiFactory.CreateRandomAndGetIdAsync(ApiClient, Faker);
+        CatId catId = await CatApiFactory.CreateRandomAndGetIdAsync(ApiClient, Faker, personId);
+        UpdateCatRequest updateCatRequest = CatApiFactory.GenerateRandomUpdateRequest(Faker);
+
+        //Act
+        HttpResponseMessage httpResponseMessage =
+            await ApiClient.Http.PutAsJsonAsync(
+                new Uri($"api/v1/cats/{catId}", UriKind.Relative), updateCatRequest);
+
+        //Assert
+        _ = await httpResponseMessage.EnsureSuccessWithDetailsAsync();
+        httpResponseMessage.StatusCode.ShouldBe(HttpStatusCode.NoContent);
+
+        CatDetailsResponse catResponse = await CatApiQueryService.GetByIdAsync(ApiClient, catId);
+        catResponse.ShouldNotBeNull();
+        catResponse.Id.ShouldBe(catId);
+        catResponse.PersonId.ShouldBe(personId);
+        catResponse.AdoptionAnnouncementId.ShouldBe(null);
+        catResponse.Name.ShouldBe(updateCatRequest.Name);
+        catResponse.Description.ShouldBe(updateCatRequest.Description);
+        catResponse.Age.ShouldBe(updateCatRequest.Age);
+        catResponse.Gender.ShouldBe(updateCatRequest.Gender);
+        catResponse.Color.ShouldBe(updateCatRequest.Color);
+        catResponse.WeightInGrams.ShouldBe(updateCatRequest.WeightInGrams);
+        catResponse.HealthStatus.ShouldBe(updateCatRequest.HealthStatus);
+        catResponse.SpecialNeedsDescription.ShouldBe(updateCatRequest.SpecialNeedsDescription);
+        catResponse.SpecialNeedsSeverityType.ShouldBe(updateCatRequest.SpecialNeedsSeverityType);
+        catResponse.Temperament.ShouldBe(updateCatRequest.Temperament);
+        catResponse.AdoptionHistoryReturnCount.ShouldBe(updateCatRequest.AdoptionHistoryReturnCount);
+        catResponse.AdoptionHistoryLastReturnDate.ShouldBe(updateCatRequest.AdoptionHistoryLastReturnDate);
+        catResponse.AdoptionHistoryLastReturnDate.ShouldBe(updateCatRequest.AdoptionHistoryLastReturnDate);
+        catResponse.AdoptionHistoryLastReturnReason.ShouldBe(updateCatRequest.AdoptionHistoryLastReturnReason);
+        catResponse.IsNeutered.ShouldBe(updateCatRequest.IsNeutered);
+        catResponse.FivStatus.ShouldBe(updateCatRequest.FivStatus);
+        catResponse.FelvStatus.ShouldBe(updateCatRequest.FelvStatus);
+        catResponse.InfectiousDiseaseStatusLastTestedAt.ShouldBe(updateCatRequest.InfectiousDiseaseStatusLastTestedAt);
+        catResponse.Vaccinations.ShouldBeEmpty();
+        catResponse.GalleryItems.ShouldBeEmpty();
+    }
+
+    [Fact]
+    public async Task UpdateCat_ShouldReturnNotFound_WhenNotExistingCatIdIsProvided()
+    {
+        //Arrange
+        UpdateCatRequest request = CatApiFactory.GenerateRandomUpdateRequest(Faker);
+
+        //Act 
+        HttpResponseMessage httpResponseMessage =
+            await ApiClient.Http.PutAsJsonAsync(
+                new Uri($"api/v1/cats/{Guid.NewGuid()}", UriKind.Relative), request);
+
+        //Assert
+        httpResponseMessage.StatusCode.ShouldBe(HttpStatusCode.NotFound);
+        ProblemDetails? problemDetails =
+            await httpResponseMessage.Content.ReadFromJsonAsync<ProblemDetails>(ApiClient.JsonOptions);
+        problemDetails.ShouldNotBeNull();
+        problemDetails.Status.ShouldBe(StatusCodes.Status404NotFound);
+    }
+}
